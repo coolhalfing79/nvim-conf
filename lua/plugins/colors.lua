87aa1c3846f0722ee104989b228320f06d84de84
coolhalfing79 <anirudhachari27@gmail.com>
2025-05-01 16:14:13 +0530

    a fresh start

--- lua/plugins/colors.lua
+++
@@ -1,6 +0,0 @@
-local MiniDeps = require('mini.deps')
-
-MiniDeps.later(function()
-    MiniDeps.add('ribru17/bamboo.nvim')
-    vim.cmd.colorscheme('bamboo')
-end)
--- lua/plugins/git.lua
+++
@@ -1,6 +0,0 @@
-local MiniDeps = require('mini.deps')
-
-MiniDeps.later(function ()
-    MiniDeps.add('tpope/vim-fugitive')
-    vim.keymap.set('n', '<leader>gw', ':tab Git<CR>', { desc = '[G]it [W]indow' })
-end)
