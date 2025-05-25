9d460dcaa748f1ae29ccb05dd55c9ff0235591cd
coolhalfing79 <anirudhachari27@gmail.com>
2025-05-25 14:25:36 +0530

    change colors again

--- init.lua
+++ init.lua
@@ -1,24 +1,28 @@
 vim.g.mapleader      = ' '
 vim.g.maplocalleader = ' '
-vim.opt.number       = true
-vim.opt.undofile     = true
-vim.opt.updatetime   = 200
-vim.opt.timeoutlen   = 250
-vim.opt.inccommand   = 'split'
-vim.opt.shiftwidth   = 4
-vim.opt.expandtab    = true
-vim.opt.completeopt  = 'menuone,noselect,popup'
-vim.opt.cmdheight    = 0
+
+vim.o.number         = true
+vim.o.undofile       = true
+vim.o.updatetime     = 250
+vim.o.timeoutlen     = 300
+vim.o.inccommand     = 'split'
+vim.o.shiftwidth     = 4
+vim.o.expandtab      = true
+vim.o.completeopt    = 'menuone,noselect,popup'
+vim.o.cmdheight      = 0
 vim.o.winborder      = 'single'
 vim.o.signcolumn     = 'yes'
 vim.o.list           = true
+
 vim.opt.listchars    = { tab = '» ', trail = '·', nbsp = '␣' }
 
 require('plugin.mini')
 require('plugin.lsp')
 require('plugin.git')
 require('plugin.telescope')
-vim.cmd.colorscheme('habamax')
+local MiniDeps = require('mini.deps')
+MiniDeps.add('bluz71/vim-moonfly-colors')
+vim.cmd.colorscheme('moonfly')
 vim.cmd([[
 hi! link TelescopePreviewNormal Pmenu
 hi! link TelescopePromptNormal Pmenu
